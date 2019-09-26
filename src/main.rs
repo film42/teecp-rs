b9325cedd61ed7ed966a19e8478c0a0054728b9c
Garrett Thornburg <film42@gmail.com>
2019-09-26 17:21:03 -0400

    It works! It can do clt->proxy,proxy->clt,clt->tee,proxy->sik

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,92 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
-//use std::io::{self, Read, Write};
+use std::io::{self, Write};
 use std::net::SocketAddr; // Shutdown
-                          //use std::sync::{Arc, Mutex};
 
-//use futures::future::join_all;
+use futures::future::join_all;
 use tokio::io::copy; // shutdown
 use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
+
+#[derive(Debug)]
+struct WriteProxy;
+
+impl WriteProxy {
+    pub fn sink() -> WriteProxy {
+        WriteProxy
+    }
+}
+
+impl Write for WriteProxy {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        println!("Sink write: {:?}", std::str::from_utf8(buf));
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+impl AsyncWrite for WriteProxy {
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        Ok(().into())
+    }
+}
+
+// Multi Writer
+#[derive(Debug)]
+struct MultiWriter<T>
+where
+    T: Sized,
+{
+    writers: Vec<Box<T>>,
+}
+
+impl<T> MultiWriter<T>
+where
+    T: AsyncWrite + Write + Send + Sync + Sized,
+{
+    pub fn new() -> MultiWriter<T> {
+        MultiWriter { writers: vec![] }
+    }
+
+    fn push(&mut self, writer: Box<T>) {
+        self.writers.push(writer);
+    }
+}
+
+impl<T> Write for MultiWriter<T>
+where
+    T: AsyncWrite + Write + Send + Sync + Sized,
+{
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        for writer in self.writers.iter_mut() {
+            let n = writer.write(buf)?;
+            if n != buf.len() {
+                return Err(io::Error::new(io::ErrorKind::Other, "Short write!"));
+            }
+        }
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        for writer in self.writers.iter_mut() {
+            writer.flush()?;
+        }
+        Ok(())
+    }
+}
+
+impl<T> AsyncWrite for MultiWriter<T>
+where
+    T: AsyncWrite + Write + Send + Sync + Sized,
+{
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        for writer in self.writers.iter_mut() {
+            writer.shutdown()?;
+        }
+        Ok(tokio::prelude::Async::Ready(()))
+    }
+}
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Config {
@@ -23,17 +101,43 @@ static EXAMPLE_CONFIG: &str = r#"
   "proxy": "127.0.0.1:6379",
   "tees": [
     "127.0.0.1:6379",
+    "127.0.0.1:6379",
+    "127.0.0.1:6379",
+    "127.0.0.1:6379",
     "127.0.0.1:6379"
   ]
 }
 "#;
 
+fn proxy_copy<R1, W1, R2, W2>(
+    src_reader: R1,
+    dest_writer: W1,
+    dest_reader: R2,
+    src_writer: W2,
+) -> tokio::prelude::future::Select2<tokio::io::Copy<R1, W1>, tokio::io::Copy<R2, W2>>
+where
+    R1: AsyncRead + Send + Sync,
+    R2: AsyncRead + Send + Sync,
+    W1: AsyncWrite + Send + Sync,
+    W2: AsyncWrite + Send + Sync,
+{
+    let src_to_dest = copy(dest_reader, src_writer);
+    let dest_to_src = copy(src_reader, dest_writer);
+    dest_to_src.select2(src_to_dest)
+}
+
 fn main() -> Result<()> {
-    //let
     let c: Config = serde_json::from_str(EXAMPLE_CONFIG)?;
     let bind_addr = c.bind.to_string().parse::<SocketAddr>().unwrap();
     let proxy_addr = c.proxy.to_string().parse::<SocketAddr>().unwrap();
     println!("{:?}, {:?}", c, proxy_addr);
+
+    let tee_addrs: Vec<SocketAddr> = c
+        .tees
+        .iter()
+        .map(|s| s.to_string().parse::<SocketAddr>().unwrap())
+        .collect();
+    println!("Tees: {:?}", tee_addrs);
 
     let listener = TcpListener::bind(&bind_addr).unwrap();
 
@@ -42,30 +146,57 @@ fn main() -> Result<()> {
         .map_err(|e| println!("Error accepting connection: {}", e))
         .for_each(move |client| {
             let proxy_conn = TcpStream::connect(&proxy_addr);
+            let mut connect_futures = vec![proxy_conn];
 
-            let lifecycle = proxy_conn.and_then(move |server| {
-                println!("{:?}", server);
+            for tee_addr in tee_addrs.iter() {
+                connect_futures.push(TcpStream::connect(tee_addr))
+            }
 
-                let (client_reader, client_writer) = client.split();
-                let (server_reader, server_writer) = server.split();
+            let lifecycle =
+                join_all(connect_futures).and_then(|mut conns| {
+                    println!("Conns: {:?}", conns);
 
-                let client_to_server = copy(server_reader, client_writer);
-                let server_to_client = copy(client_reader, server_writer);
+                    let (clt_reader, clt_writer) = client.split();
+                    let srv_stream = conns.remove(0);
+                    let (srv_reader, srv_writer) = srv_stream.split();
 
-                server_to_client.select(client_to_server).then(|x| {
-                    x.map(|_| println!("It completed!"))
-                        .map_err(|err| eprintln!("An error occurred during the proxy: {:?}", err))
-                        .expect("never errors");
+                    let mut multi_writer = MultiWriter::new();
+                    multi_writer.push(Box::new(srv_writer));
 
-                    // Do this because the type system doesn't want an io::Error here?
-                    future::ok(())
-                })
-            });
+                    let tee_reader_copy_futures: Vec<
+                        tokio::io::Copy<tokio::io::ReadHalf<TcpStream>, WriteProxy>,
+                    > = conns
+                        .drain(..)
+                        .map(|tee_conn| {
+                            let (tee_reader, tee_writer) = tee_conn.split();
+                            multi_writer.push(Box::new(tee_writer));
+                            copy(tee_reader, WriteProxy::sink())
+                        })
+                        .collect();
+
+                    proxy_copy(
+                        Box::new(clt_reader),
+                        Box::new(multi_writer),
+                        Box::new(srv_reader),
+                        Box::new(clt_writer),
+                    )
+                    .select2(tokio::prelude::future::select_all(tee_reader_copy_futures))
+                    .then(|x| {
+                        x.map(|_| println!("It completed!"))
+                            .map_err(|err| {
+                                eprintln!("An error occurred during the proxy: {:?}", err)
+                            })
+                            .expect("never errors");
+
+                        // Do this because the type system doesn't want an io::Error here?
+                        future::ok(())
+                    })
+                });
 
             tokio::spawn(
                 lifecycle
                     .map(|_| println!("The proxy is done"))
-                    .map_err(|err| eprintln!("Proxy err: {}", err)),
+                    .map_err(|err| eprintln!("Proxy err: {:?}", err)),
             )
         });
 
@@ -73,19 +204,60 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-// struct SinkWriter;
+// fn proxy(
+//     src: TcpStream,
+//     dest: TcpStream,
+// ) -> tokio::prelude::future::Select<
+//     tokio::io::Copy<tokio::io::ReadHalf<TcpStream>, tokio::io::WriteHalf<TcpStream>>,
+//     tokio::io::Copy<tokio::io::ReadHalf<TcpStream>, tokio::io::WriteHalf<TcpStream>>,
+// > {
+//     let (src_reader, src_writer) = src.split();
+//     let (dest_reader, dest_writer) = dest.split();
+//     let src_to_dest = copy(dest_reader, src_writer);
+//     let dest_to_src = copy(src_reader, dest_writer);
+//     dest_to_src.select(src_to_dest)
+// }
 
-// impl Write for SinkWriter {
-//     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-//         Ok(buf.len())
+// #[derive(Debug)]
+// struct WriteProxy<T> {
+//     writer: Option<T>,
+// }
+
+// impl<T> WriteProxy<T>
+// where
+//     T: AsyncWrite + Write + Send + Sync,
+// {
+//     pub fn sink() -> WriteProxy<T> {
+//         WriteProxy { writer: None }
 //     }
-//     fn flush(&mut self) -> io::Result<()> {
-//         Ok(())
+
+//     pub fn new(writer: T) -> WriteProxy<T> {
+//         WriteProxy {
+//             writer: Some(writer),
+//         }
 //     }
 // }
-// impl AsyncWrite for SinkWriter {
+
+// impl<T: Write> Write for WriteProxy<T> {
+//     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+//         match &mut self.writer {
+//             Some(ref mut writer) => writer.write(buf),
+//             None => Ok(buf.len()),
+//         }
+//     }
+//     fn flush(&mut self) -> io::Result<()> {
+//         match &mut self.writer {
+//             Some(ref mut writer) => writer.flush(),
+//             None => Ok(()),
+//         }
+//     }
+// }
+// impl<T: AsyncWrite> AsyncWrite for WriteProxy<T> {
 //     fn shutdown(&mut self) -> Poll<(), io::Error> {
-//         Ok(().into())
+//         match &mut self.writer {
+//             Some(ref mut writer) => writer.shutdown(),
+//             None => Ok(().into()),
+//         }
 //     }
 // }
 
